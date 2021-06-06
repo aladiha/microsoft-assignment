0590d5cd9eedf72e6eeec7ebfbd5df1a3d611461
aladiha <aladin.handuklo@gmail.com>
2021-06-06 20:43:00 +0300

    run script in vm

--- powershell.ps1
+++ powershell.ps1
@@ -1,6 +1,7 @@
 
-Connect-AzAccount
-<#
+#connect to account
+Connect-AzAccount -UseDeviceAuthentication
+
 # select location
 Get-AzLocation | select Location
 $location = "eastus"
@@ -8,29 +9,37 @@ $location = "eastus"
 #create resource group
 $resourceGroup = "myResourceGroup"
 New-AzResourceGroup -Name $resourceGroup -Location $location
-#>
+
+
+# get storage accountes
 $storageAccount = Get-AzStorageAccount -ResourceGroupName "myResourceGroup" -Name "store1mystorage03062021"
 $storageAccountb = Get-AzStorageAccount -ResourceGroupName "myResourceGroup" -Name "store2mystorage03062021"
-
 $ctx = $storageAccount.Context
 $ctxb = $storageAccountb.Context
 
-$containerName = "test1"
+#create containers
+$containerName = "test3"
 New-AzStorageContainer -Name $containerName -Context $ctx -Permission blob
-
-$containerNameB = "test2"
+$containerNameB = "test3"
 New-AzStorageContainer -Name $containerNameB -Context $ctxb -Permission blob
 
+# create files in VM machine
+for ($num = 1 ; $num -le 100 ; $num++){
+    [string]$index = $num.ToString()
+    New-Item "\home\azureuser\files\$index.txt"
+    Set-Content "\home\azureuser\files\$index.txt" "file number $index"
+}
 
+#upload files to azure sorage account
 for ($num = 1 ; $num -le 100 ; $num++){
   [string]$index = $num.ToString()
-  Set-AzStorageBlobContent -File "/Users/aladinhandoklo/Desktop/files/$index.txt" `
+  Set-AzStorageBlobContent -File "/home/azureuser/files/$index.txt" `
     -Container $containerName `
     -Blob "blob$index.txt" `
     -Context $ctx 
 }
 
-
+#copy files from one storage to another
 Get-AzStorageBlob -Context $ctx `
 -Container $containerName | Start-AzStorageBlobCopy -DestContext $ctxb `
 -DestContainer $containerNameB
